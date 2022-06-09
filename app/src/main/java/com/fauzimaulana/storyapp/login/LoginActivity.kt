1d7945641bfdbb7ef11d9c5a7559aab162936084
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2022-06-09 17:00:16 +0700

    Splash Screen added to the app

--- app/src/main/java/com/fauzimaulana/storyapp/login/LoginActivity.kt
+++ app/src/main/java/com/fauzimaulana/storyapp/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.fauzimaulana.storyapp
+package com.fauzimaulana.storyapp.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fauzimaulana.storyapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
