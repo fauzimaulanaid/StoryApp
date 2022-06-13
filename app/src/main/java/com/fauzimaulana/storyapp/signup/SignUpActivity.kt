ced670178655f066bb773de0c0a45294d7b10119
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2022-06-13 23:46:40 +0700

    Add property animation to sign up page

--- app/src/main/java/com/fauzimaulana/storyapp/signup/SignUpActivity.kt
+++ app/src/main/java/com/fauzimaulana/storyapp/signup/SignUpActivity.kt
@@ -1,8 +1,11 @@
 package com.fauzimaulana.storyapp.signup
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.fauzimaulana.storyapp.R
@@ -18,6 +21,7 @@ class SignUpActivity : AppCompatActivity() {
         _binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+        playAnimation()
     }
 
     private fun setupView() {
@@ -31,5 +35,24 @@ class SignUpActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+    }
+
+    private fun playAnimation() {
+        val image = ObjectAnimator.ofFloat(binding.imageView, View.ALPHA, 1f).setDuration(500)
+        val signup = ObjectAnimator.ofFloat(binding.signupTextView, View.ALPHA, 1f).setDuration(500)
+        val name = ObjectAnimator.ofFloat(binding.nameTextView, View.ALPHA, 1f).setDuration(500)
+        val nameInput = ObjectAnimator.ofFloat(binding.nameEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val email = ObjectAnimator.ofFloat(binding.emailTextView, View.ALPHA, 1f).setDuration(500)
+        val emailInput = ObjectAnimator.ofFloat(binding.emailEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val password = ObjectAnimator.ofFloat(binding.passwordTextView, View.ALPHA, 1f).setDuration(500)
+        val passwordInput = ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val signupButton = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(500)
+        val copyright = ObjectAnimator.ofFloat(binding.copyright, View.ALPHA, 1f).setDuration(500)
+
+        AnimatorSet().apply {
+            playSequentially(image, signup, name, nameInput, email, emailInput, password, passwordInput, signupButton, copyright)
+            startDelay = 500
+            start()
+        }
     }
 }
