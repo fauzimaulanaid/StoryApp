9132e69a5a24ae8cbc5f9f2dec0bc0370ba3f7ab
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2022-06-13 23:36:02 +0700

    Layouting sign up page

--- app/src/main/java/com/fauzimaulana/storyapp/signup/SignUpActivity.kt
+++ app/src/main/java/com/fauzimaulana/storyapp/signup/SignUpActivity.kt
@@ -1,12 +1,35 @@
 package com.fauzimaulana.storyapp.signup
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.fauzimaulana.storyapp.R
+import com.fauzimaulana.storyapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private var _binding: ActivitySignUpBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        _binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
