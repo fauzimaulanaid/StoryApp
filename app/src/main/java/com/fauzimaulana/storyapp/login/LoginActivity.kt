071ab8495d7d71a70b91309fb223cee9ee1e71f3
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2022-06-13 22:16:23 +0700

    Layouting login page

--- app/src/main/java/com/fauzimaulana/storyapp/login/LoginActivity.kt
+++ app/src/main/java/com/fauzimaulana/storyapp/login/LoginActivity.kt
@@ -1,13 +1,13 @@
 package com.fauzimaulana.storyapp.login
 
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.window.SplashScreen
-import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.fauzimaulana.storyapp.R
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.fauzimaulana.storyapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -19,5 +19,37 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupView()
+        playAnimation()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun playAnimation() {
+        val image = ObjectAnimator.ofFloat(binding.imageViewStory, View.ALPHA, 1f).setDuration(500)
+        val login = ObjectAnimator.ofFloat(binding.loginTextView, View.ALPHA, 1f).setDuration(500)
+        val email = ObjectAnimator.ofFloat(binding.emailTextView, View.ALPHA, 1f).setDuration(500)
+        val emailInput = ObjectAnimator.ofFloat(binding.emailEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val password = ObjectAnimator.ofFloat(binding.passwordTextView, View.ALPHA, 1f).setDuration(500)
+        val passwordInput = ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val loginButton = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(500)
+        val copyright = ObjectAnimator.ofFloat(binding.copyright, View.ALPHA, 1f).setDuration(500)
+
+        AnimatorSet().apply {
+            playSequentially(image, login, email, emailInput, password, passwordInput, loginButton, copyright)
+            startDelay = 500
+            start()
+        }
     }
 }
