d1aa3624b71e5542c70e8f1d1397f3d1e21bff04
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2022-06-17 16:07:10 +0700

    Add try catch in RemoteDataSource

--- app/src/main/java/com/fauzimaulana/storyapp/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/fauzimaulana/storyapp/core/data/source/remote/RemoteDataSource.kt
@@ -12,42 +12,57 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import java.lang.Exception
+import kotlin.Exception
 
 class RemoteDataSource(private val apiService: ApiService) {
 
     fun registerUser(name: String, email: String, password: String): Flow<ApiResponse<SignUpResponse>> {
         return flow {
-            val response = apiService.registerUser(name, email, password)
-            if (response.error) {
-                emit(ApiResponse.Error(response.message))
-                Log.e("RemoteDataSource: ", response.message)
-            } else {
-                emit(ApiResponse.Success(response))
+            try {
+                val response = apiService.registerUser(name, email, password)
+                if (response.error) {
+                    emit(ApiResponse.Error(response.message))
+                    Log.e("RemoteDataSource: ", response.message)
+                } else {
+                    emit(ApiResponse.Success(response))
+                }
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+                Log.e("RemoteDataSource: ", e.toString())
             }
         }.flowOn(Dispatchers.IO)
     }
 
     fun userLogin(email: String, password: String): Flow<ApiResponse<LoginResult>> {
         return flow {
-            val response = apiService.userLogin(email, password)
-            if (response.error) {
-                emit(ApiResponse.Error(response.message))
-                Log.e("RemoteDataSource: ", response.message)
-            } else {
-                emit(ApiResponse.Success(response.loginResult))
+            try {
+                val response = apiService.userLogin(email, password)
+                if (response.error) {
+                    emit(ApiResponse.Error(response.message))
+                    Log.e("RemoteDataSource: ", response.message)
+                } else {
+                    emit(ApiResponse.Success(response.loginResult))
+                }
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+                Log.e("RemoteDataSource: ", e.toString())
             }
         }.flowOn(Dispatchers.IO)
     }
 
     fun uploadStory(file: MultipartBody.Part, description: RequestBody, token: String): Flow<ApiResponse<AddNewStoryResponse>> {
         return flow {
-            val response = apiService.uploadStory(file, description, "Bearer $token")
-            if (response.error) {
-                emit(ApiResponse.Error(response.message))
-                Log.e("RemoteDataSource: ", response.message)
-            } else {
-                emit(ApiResponse.Success(response))
+            try {
+                val response = apiService.uploadStory(file, description, "Bearer $token")
+                if (response.error) {
+                    emit(ApiResponse.Error(response.message))
+                    Log.e("RemoteDataSource: ", response.message)
+                } else {
+                    emit(ApiResponse.Success(response))
+                }
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+                Log.e("RemoteDataSource: ", e.toString())
             }
         }.flowOn(Dispatchers.IO)
     }
