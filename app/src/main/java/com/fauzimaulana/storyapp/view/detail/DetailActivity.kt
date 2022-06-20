6ba251e10c7c53901c741a24cb7f237d0abf731c
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2022-06-20 16:39:03 +0700

    UI Updated for bottom navigation and detail activity

--- app/src/main/java/com/fauzimaulana/storyapp/view/detail/DetailActivity.kt
+++ app/src/main/java/com/fauzimaulana/storyapp/view/detail/DetailActivity.kt
@@ -8,6 +8,7 @@ import com.fauzimaulana.storyapp.R
 import com.fauzimaulana.storyapp.core.domain.model.StoryModel
 import com.fauzimaulana.storyapp.databinding.ActivityDetailBinding
 import com.fauzimaulana.storyapp.databinding.ContentDetailBinding
+import java.text.SimpleDateFormat
 
 class DetailActivity : AppCompatActivity() {
 
@@ -28,6 +29,7 @@ class DetailActivity : AppCompatActivity() {
         showDetailStory(story, createdAt!!)
     }
 
+    @Suppress("SimpleDateFormat", "DEPRECATION")
     private fun showDetailStory(story: StoryModel?, createdAt: String) {
         story?.let {
             binding.tvName.text = story.name
@@ -38,7 +40,44 @@ class DetailActivity : AppCompatActivity() {
                     .error(R.drawable.ic_error))
                 .into(binding.storyImageView)
             binding.tvDescription.text = story.description
-            binding.tvDateCreated.text = story.createAt
+            val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            val date = formatter.parse(story.createAt)
+
+            val day = when(date!!.day) {
+                0 -> "Sunday"
+                1 -> "Monday"
+                2 -> "Tuesday"
+                3 -> "Wednesday"
+                4 -> "Thursday"
+                5 -> "Friday"
+                6 -> "Saturday"
+                else -> "Unknown"
+            }
+
+            val dateCreated = date.date
+
+            val month = when(date.month) {
+                0 -> "January"
+                1 -> "February"
+                2 -> "March"
+                3 -> "April"
+                4 -> "May"
+                5 -> "June"
+                6 -> "July"
+                7 -> "August"
+                8 -> "September"
+                9 -> "October"
+                10 -> "November"
+                11 -> "December"
+                else -> "Unknown"
+            }
+
+            val year = date.year % 100
+
+            val hours = if (date.hours.toString().length == 1) "0${date.hours}" else date.hours
+            val minutes = if (date.minutes.toString().length == 1) "0${date.minutes}" else date.minutes
+
+            binding.tvDateCreated.text = "$day, $dateCreated $month 20$year at $hours.$minutes WIB"
             binding.tvCreatedFrom.text = resources.getString(R.string.created_from, story.lat, story.lon)
         }
     }
