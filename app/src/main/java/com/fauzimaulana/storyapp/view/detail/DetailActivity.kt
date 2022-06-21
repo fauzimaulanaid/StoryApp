f9c96520007a5c483f54ec8f5d41ec74c38a74ab
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2022-06-21 12:36:42 +0700

    Fix layouting on Detail Activity

--- app/src/main/java/com/fauzimaulana/storyapp/view/detail/DetailActivity.kt
+++ app/src/main/java/com/fauzimaulana/storyapp/view/detail/DetailActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.ceylonlabs.imageviewpopup.ImagePopup
 import com.fauzimaulana.storyapp.R
 import com.fauzimaulana.storyapp.core.domain.model.StoryModel
 import com.fauzimaulana.storyapp.databinding.ActivityDetailBinding
 import com.fauzimaulana.storyapp.databinding.ContentDetailBinding
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -27,6 +31,12 @@ class DetailActivity : AppCompatActivity() {
         val story = intent.getParcelableExtra<StoryModel>(EXTRA_DATA)
         val createdAt = intent.extras?.getString(EXTRA_INFO)
         showDetailStory(story, createdAt!!)
+
+        val imagePopUp = ImagePopup(this)
+        imagePopUp.initiatePopupWithGlide(story?.photoUrl)
+        binding.storyImageView.setOnClickListener {
+            imagePopUp.viewPopup()
+        }
     }
 
     @Suppress("SimpleDateFormat", "DEPRECATION")
@@ -42,44 +52,54 @@ class DetailActivity : AppCompatActivity() {
             binding.tvDescription.text = story.description
             val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
             val date = formatter.parse(story.createAt)
+            val calendar = Calendar.getInstance()
+            calendar.time = date!!
+            calendar.add(Calendar.HOUR, 7)
 
-            val day = when(date!!.day) {
-                0 -> "Sunday"
-                1 -> "Monday"
-                2 -> "Tuesday"
-                3 -> "Wednesday"
-                4 -> "Thursday"
-                5 -> "Friday"
-                6 -> "Saturday"
-                else -> "Unknown"
-            }
+            binding.tvDateCreated.text = resources.getString(R.string.date_created_wib, createDate(calendar.time))
+            binding.tvDateCreatedOnServer.text = resources.getString(R.string.date_created_server_time, createDate(date))
 
-            val dateCreated = date.date
-
-            val month = when(date.month) {
-                0 -> "January"
-                1 -> "February"
-                2 -> "March"
-                3 -> "April"
-                4 -> "May"
-                5 -> "June"
-                6 -> "July"
-                7 -> "August"
-                8 -> "September"
-                9 -> "October"
-                10 -> "November"
-                11 -> "December"
-                else -> "Unknown"
-            }
-
-            val year = date.year % 100
-
-            val hours = if (date.hours.toString().length == 1) "0${date.hours}" else date.hours
-            val minutes = if (date.minutes.toString().length == 1) "0${date.minutes}" else date.minutes
-
-            binding.tvDateCreated.text = "$day, $dateCreated $month 20$year at $hours.$minutes WIB"
             binding.tvCreatedFrom.text = resources.getString(R.string.created_from, story.lat, story.lon)
         }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun createDate(date: Date): String {
+        val day = when(date.day) {
+            0 -> "Sun"
+            1 -> "Mon"
+            2 -> "Tue"
+            3 -> "Wed"
+            4 -> "Thu"
+            5 -> "Fri"
+            6 -> "Sat"
+            else -> "Unknown"
+        }
+
+        val month = when(date.month) {
+            0 -> "Jan"
+            1 -> "Feb"
+            2 -> "Mar"
+            3 -> "Apr"
+            4 -> "May"
+            5 -> "Jun"
+            6 -> "Jul"
+            7 -> "Aug"
+            8 -> "Sep"
+            9 -> "Oct"
+            10 -> "Nov"
+            11 -> "Dec"
+            else -> "Unknown"
+        }
+
+        val dateCreated = date.date
+
+        val year = date.year % 100
+
+        val hours = if (date.hours.toString().length == 1) "0${date.hours}" else date.hours
+        val minutes = if (date.minutes.toString().length == 1) "0${date.minutes}" else date.minutes
+
+        return "$day $month $dateCreated 20$year at $hours.$minutes"
     }
 
     override fun onSupportNavigateUp(): Boolean {
